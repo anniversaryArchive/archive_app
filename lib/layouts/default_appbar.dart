fe609a73b69a84297d812d626663a07a94c106e3
kimyejin <7602kyj@e-mirim.hs.kr>
2022-07-06 17:53:26 +0900

    feat: appbar title 받아올 수 있게 구현

---
+++ lib/layouts/default_appbar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DefaultAppbar extends StatelessWidget implements PreferredSizeWidget {
+  Size get preferredSize => Size.fromHeight(50);
+
+  final String title;
+
+  const DefaultAppbar({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.black,
+      elevation: 0.0,
+      title: Text(
+        '${title}',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          letterSpacing: 0.6,
+        ),
+      ),
+    );
+  }
+}
