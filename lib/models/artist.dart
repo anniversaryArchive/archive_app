fa3ac149b67a3e84dc3bfdfc5bd92466f4029b92
kimyejin <7602kyj@e-mirim.hs.kr>
2022-07-08 00:43:40 +0900

    feat: model json 직렬화 작업중

---
+++ lib/models/artist.dart
@@ -0,0 +1,30 @@
+// import 'package:archive/models/group.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'artist.g.dart';
+
+@JsonSerializable()
+class Artist {
+
+  final String name; // 아티스트 명
+  final DateTime createdAt; // 생성 일자
+  final DateTime? updatedAt; // 마지막 업데이트 일자
+  final DateTime debutDate; // 데뷔일
+  final DateTime birthDay; // 생일
+  // final Group? group; // 아티스트가 속한 그룹
+
+  Artist({
+    required this.name,
+    required this.createdAt,
+    this.updatedAt,
+    required this.debutDate,
+    required this.birthDay,
+    // this.group,
+  });
+
+  factory Artist.fromJson(Object? json) =>
+      _$ArtistFromJson(json as Map<String, dynamic>);
+
+  Map<String, dynamic> toJson() =>
+      _$ArtistToJson(this);
+}
