8445a48225bb082146c39a3067dfb2ae9756d562
kimyejin <7602yejin@gmail.com>
2022-07-20 14:16:12 +0900

    feat: 그룹 내 아티스트 item UI구현

--- lib/screens/select-artist-in-group/components/select-group-artist-item.dart
+++ lib/screens/select-artist-in-group/components/select-group-artist-item.dart
@@ -4,30 +4,57 @@ import 'package:flutter/material.dart';
 
 class SelectGroupArtistItem extends StatelessWidget {
   final Artist groupArtist;
+  final Function(Artist) onClick;
 
-  const SelectGroupArtistItem({Key? key, required this.groupArtist}) : super(key: key);
+  const SelectGroupArtistItem({Key? key, required this.groupArtist, required this.onClick}) : super(key: key);
+
+  Widget _artistImageWidget() {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+      child: groupArtist.imagePath != null
+          ? ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: Image.network(groupArtist.imagePath!),
+            )
+          : Container(
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+            ),
+    );
+  }
+
+  Widget _artistInfoWidget() {
+    return Container(
+      margin: EdgeInsets.only(left: 10.0),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(groupArtist.name),
+            Text(formatDate(groupArtist.birthDay)),
+          ],
+        ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: groupArtist.imagePath != null
-                ? Image.network(groupArtist.imagePath!)
-                : Container(
-                    color: Colors.grey,
-                  ),
-          ),
-          Column(
-            children: [
-              Text(groupArtist.name),
-              Text(formatDate(groupArtist.birthDay)),
-            ],
-          ),
-        ],
+      onTap: () => onClick(groupArtist),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 11.0),
+        child: Row(
+          children: [
+            _artistImageWidget(),
+            _artistInfoWidget(),
+          ],
+        ),
       ),
     );
   }
