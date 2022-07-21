915efb04efc6b39abe405c94d2fe50ace5bcf85d
kimyejin <7602yejin@gmail.com>
2022-07-21 15:42:17 +0900

    fix: 'select-member' 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,6 @@ class _MyAppState extends State<MyApp> {
   List<GetPage> _getPages () {
     return [
       GetPage(name: '/', page: () => const HomeScreen()),
-      GetPage(name: '/select-member', page: () => const HomeScreen()),
       GetPage(name: '/select-artist', page: () => const SelectArtistScreen()),
       GetPage(name: '/select-group-artist', page: () => SelectGroupArtistScreen()),
     ];
